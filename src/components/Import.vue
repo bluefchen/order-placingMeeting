2adb7b52d20378edb8b909ed1218e089fe57c8d9
dingshaohua <345561171@qq.com>
2018-05-02 20:24:40 +0800

    bugfix

--- src/components/Import.vue
+++ src/components/Import.vue
@@ -22,92 +22,17 @@
 
   export default {
     name: 'Import',
-    props: {},
+    props: {
+      tableTitle: {
+        type: Array,
+        require: true
+      }
+    },
     data() {
       return {
         totalCnt: 0,
         successCnt: 0,
         failCnt: 0,
-        tableTitle: [{
-          label: '终端编码',
-          prop: 'offerCode',
-          width: 160
-        }, {
-          label: '终端名称',
-          prop: 'offerName',
-          width: 120
-        }, {
-          label: '终端品牌',
-          prop: 'brandName',
-          width: 100
-        }, {
-          label: '终端型号',
-          prop: 'offerModelName',
-          width: 140
-        }, {
-          label: '产品类型',
-          prop: 'isCentman',
-          width: 100,
-          render: (h, params) => {
-            return h({
-              template: '<div><span v-if="data.row.isCentman === \'Y\'">集采</span><span v-else>社采</span></div>',
-              data() {
-                return {
-                  data: params
-                }
-              }
-            });
-          }
-        }, {
-          label: '终端价格',
-          prop: 'costPrice',
-          width: 100,
-          render: (h, params) => {
-            return h({
-              template: '<span>￥{{data.row.costPrice}}</span>',
-              data() {
-                return {
-                  data: params
-                }
-              }
-            });
-          }
-        }, {
-          label: '是否特种机型',
-          prop: 'isSpecial',
-          width: 100,
-          render: (h, params) => {
-            return h({
-              template: '<span class="state-icon" :class="data.row.isSpecial === \'Y\' ? \'ok\' : \'error\'"></span>',
-              data() {
-                return {
-                  data: params
-                }
-              }
-            });
-          }
-        }, {
-          label: '数量',
-          prop: 'offerQty',
-          width: 100
-        }, {
-          label: '校验结果',
-          prop: 'isSuccess',
-          width: 100,
-          render: (h, params) => {
-            return h({
-              template: '<div><span class="result-icon" v-if="data.row.isSuccess === \'Y\'"></span><span v-else>--</span></div>',
-              data() {
-                return {
-                  data: params
-                }
-              }
-            });
-          }
-        }, {
-          label: '校验信息',
-          prop: 'resultMsg'
-        }],
         tableData: []
       }
     },
