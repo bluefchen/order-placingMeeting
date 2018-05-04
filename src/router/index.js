6ff46a85a460b5214bbf10e9f0b95aff5a865cab
dingshaohua <345561171@qq.com>
2018-05-04 14:21:48 +0800

    bugfix

--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,7 @@ const PolicyManage = () => import('@/views/PolicyManage');
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
+  // mode: 'history',
   routes: [
     {
       path: '/',
