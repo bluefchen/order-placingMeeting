f568c90a04be10bc56804753c54b1a694b3a88d5
heyue <289383680@qq.com>
2018-05-08 14:44:33 +0800

    bugfix

--- src/views/SpecialModel.vue
+++ src/views/SpecialModel.vue
@@ -356,12 +356,27 @@
         this.queryOpmOfferAllotList();
       },
       deleteOpmOfferAllot(val, index){
-        this.$post('/orderPlacingMeetingController/deleteOpmOfferAllot', {
-          opmOaId: val.opmOaId
-        }).then((rsp) => {
-          console.log('删除成功！');
+        this.$confirm('确定要删除该机型吗?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$post('/orderPlacingMeetingController/deleteOpmOfferAllot', {
+            opmOaId: val.opmOaId
+          }).then((rsp) => {
+            console.log('删除成功！');
+          });
+          this.$message({
+            type: 'success',
+            message: '删除成功!'
+          });
+          this.queryOpmOfferAllotList(this.currentPage);
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          });          
         });
-        this.queryOpmOfferAllotList(this.currentPage);
       }
     },
     components: {
