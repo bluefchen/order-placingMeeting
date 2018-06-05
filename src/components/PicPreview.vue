63dcfaba41cf32ef54efb1b5926834f3438bff6a
dingshaohua <345561171@qq.com>
2018-06-05 10:09:26 +0800

    bugfix

--- src/components/PicPreview.vue
+++ src/components/PicPreview.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="pic-zoom">
-      <PicZoom :url="url" :bigUrl="bigUrl"/>
+      <PicZoom :url="url"/>
     </div>
     <div class="slide fn-clear">
       <button class="slide-left fn-left" @click="scrollLeft" :disabled="animateLeft">
@@ -9,7 +9,8 @@
       </button>
       <div class="slide-center fn-left">
         <ul :class="{'animate-left': animateLeft, 'animate-right': animateRight}">
-          <li v-for="(item, index) in imgList" v-on:mouseover="checkedUrl(item, index)" :class="{'checked': index === checkedIndex}" v-show="index >= showMinIndex && index <= showMaxIndex">
+          <li v-for="(item, index) in imgList" v-on:mouseover="checkedUrl(item, index)"
+              :class="{'checked': index === checkedIndex}" v-show="index >= showMinIndex && index <= showMaxIndex">
             <img :src="item" alt="">
           </li>
         </ul>
@@ -28,30 +29,24 @@
     name: 'PicPreview',
     created() {
       //取图片列表最后一项，放到数组最前面。ul往左偏移一项显示，防止点击左右按钮切换时候出现突然显示问题。自动选择index为1的项
-      if(this.imgList.length){
-        var imgItem = this.imgList.pop();
+      if (this.imgList.length) {
+        let imgItem = this.imgList.pop();
         this.imgList.unshift(imgItem);
         this.imgList.push(imgItem);
         this.checkedUrl(this.imgList[1], 1);
-      };
+      }
     },
     data() {
       return {
         imgList: [
-          'http://img12.360buyimg.com/n1/s450x450_jfs/t17149/173/1779654602/189601/335d3d90/5ad8628dN03dc292f.jpg'
-        ,
-          'http://img10.360buyimg.com/n1/s450x450_jfs/t17773/222/1772357349/201925/627b888c/5ad8614bN7b9187f7.jpg'
-        ,
-          'http://img10.360buyimg.com/n1/s450x450_jfs/t18157/222/1822300674/231514/6c179af8/5ad87390N086a3c91.jpg'
-        ,
-          'http://img12.360buyimg.com/n1/s450x450_jfs/t17086/355/1787289240/85381/8ed3a44/5ad86285Nc411d600.jpg'
-        ,
-          'http://img14.360buyimg.com/n1/s450x450_jfs/t17494/75/1869981719/216755/afe6bf7/5ad87250N8b8f157e.jpg'
-        ,
+          'http://img12.360buyimg.com/n1/s450x450_jfs/t17149/173/1779654602/189601/335d3d90/5ad8628dN03dc292f.jpg',
+          'http://img10.360buyimg.com/n1/s450x450_jfs/t17773/222/1772357349/201925/627b888c/5ad8614bN7b9187f7.jpg',
+          'http://img10.360buyimg.com/n1/s450x450_jfs/t18157/222/1822300674/231514/6c179af8/5ad87390N086a3c91.jpg',
+          'http://img12.360buyimg.com/n1/s450x450_jfs/t17086/355/1787289240/85381/8ed3a44/5ad86285Nc411d600.jpg',
+          'http://img14.360buyimg.com/n1/s450x450_jfs/t17494/75/1869981719/216755/afe6bf7/5ad87250N8b8f157e.jpg',
           'http://img10.360buyimg.com/n1/s450x450_jfs/t18076/23/1790973070/197564/1c7e24b3/5ad87379N59492ad3.jpg'
         ],
         url: '',
-        bigUrl: '',
         checkedIndex: '',
         animateLeft: false, //往左切换的动画样式
         animateRight: false, //向右切换的动画样式
@@ -61,30 +56,29 @@
       }
     },
     methods: {
-      checkedUrl(item, index){
+      checkedUrl(item, index) {
         this.url = item;
-        this.bigUrl = item;
         this.checkedIndex = index;
       },
-      scrollLeft(){
-        if(this.showMinIndex > 0){
+      scrollLeft() {
+        if (this.showMinIndex > 0) {
           this.animateLeft = true;
           setTimeout(() => {
             this.showMinIndex = this.showMinIndex - 1;
             this.showMaxIndex = this.showMaxIndex - 1;
             this.animateLeft = false;
           }, 500)
-        };
+        }
       },
-      scrollRight(){
-        if(this.showMinIndex + this.showIndexLength < this.imgList.length){
+      scrollRight() {
+        if (this.showMinIndex + this.showIndexLength < this.imgList.length) {
           this.animateRight = true;
           setTimeout(() => {
             this.showMinIndex = this.showMinIndex + 1;
             this.showMaxIndex = this.showMaxIndex + 1;
-            this.animateRight= false;
+            this.animateRight = false;
           }, 500)
-        };
+        }
       }
     },
     components: {
@@ -99,10 +93,11 @@
     height: 390px;
     border: 1px solid #ddd;
   }
-  .slide{
+
+  .slide {
     margin-top: 20px;
     width: 100%;
-    .slide-left, .slide-right{
+    .slide-left, .slide-right {
       position: relative;
       width: 19px;
       height: 72px;
@@ -111,23 +106,23 @@
       line-height: 72px;
       z-index: 999;
       cursor: pointer;
-      img{
+      img {
         vertical-align: middle;
       }
     }
-    .slide-center{
+    .slide-center {
       position: relative;
       margin-left: 15px;
       width: 325px;
       height: 72px;
       overflow: hidden;
-      ul{
+      ul {
         position: absolute;
         left: -82px;
         top: 0;
         width: 160%;
         height: 72px;
-        li{
+        li {
           float: left;
           display: flex;
           width: 68px;
@@ -137,19 +132,19 @@
           cursor: pointer;
           align-items: center;
           justify-content: center;
-          img{
+          img {
             max-width: 100%;
             max-height: 100%;
           }
-          &.checked{
+          &.checked {
             border: 2px solid #f00;
           }
         }
-        &.animate-left{
+        &.animate-left {
           transition: all 0.5s;
           left: 0;
         }
-        &.animate-right{
+        &.animate-right {
           transition: all 0.5s;
           left: -164px;
         }
