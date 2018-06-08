c3d1b3333a319fd594b1fc1be1b13b702983a409
heyue <289383680@qq.com>
2018-06-08 11:01:26 +0800

    bugfix

--- src/views/OrderPickingDetail.vue
+++ src/views/OrderPickingDetail.vue
@@ -15,7 +15,7 @@
     <!-- 我的位置 -->
     <div class="my-location">
       <div class="box-1200">
-        <Breadcrumb :list="['首页', '统计查询', '订单查询']"/>
+        <Breadcrumb :list="['首页', '统计查询', '订单提货上报', '江苏省订单提货清单明细']"/>
       </div>
     </div>
 
@@ -60,7 +60,7 @@
 
     <div class="box-1200 tabs-list">
       <div class="order-titl fn-clear">
-        <TitlePlate class="fn-left" title="订单列表"/>
+        <TitlePlate class="fn-left" title="订单提货清单明细"/>
       </div>
       <div class="result-table">
         <Table :border="false" :tableTitle="tableTitle" :tableData="tableData"/>
@@ -73,57 +73,83 @@
 <script>
   import Breadcrumb from '@/components/Breadcrumb';
   import Input from '@/components/Input';
-  import Select from '@/components/Select';
   import InputWithSelect from '@/components/InputWithSelect';
   import TitlePlate from '@/components/TitlePlate';
   import Table from '@/components/Table';
-  import DeviceInfo from '@/components/DeviceInfo';
   import Pagination from '@/components/Pagination';
   import ChooseMerchants from '@/components/ChooseMerchants';
 
   export default {
     name: 'OrderPickingDetail',
     created() {
+      this.opMeetingInfo = JSON.parse(localStorage.getItem('opMeeting'));
       this.qryOpmOrderPickupInfo();
     },
     data() {
       return {
         tableTitle: [{
-          label: '省份',
-          prop: 'commonRegionName'
+          label: '订单号',
+          prop: 'opmOrderNo',
+          width: 120,
         }, {
-          label: '订单数量',
-          prop: 'offerQty'
+          label: '零售商',
+          prop: 'retailerName',
+          width: 120,
         }, {
-          label: '订单提货上报量',
-          prop: 'pickupGoodsAmount',
+          label: '终端编码',
+          prop: 'offerCode',
+          width: 120,
+        }, {
+          label: '终端名称',
+          prop: 'offerName'
+        }, {
+          label: '终端品牌',
+          prop: 'brandName',
+          width: 100
+        }, {
+          label: '终端型号',
+          prop: 'offerModelName',
+          width: 100
+        }, {
+          label: '产品类型',
+          prop: 'isCentman',
+          width: 80,
           render: (h, params) => {
             return h({
-              template: '<div class="link-btn" @click="qryOrderPickupInfo(data.row)"><a>{{data.row.pickupGoodsAmount}}</a></div>',
+              template: '<div><span v-if="data.row.isCentman === \'Y\'">集采</span><span v-else>社采</span></div>',
               data() {
                 return {
                   data: params
                 }
-              },
-              methods: {
-                qryOrderPickupInfo(item) {
-                  // this.$router.push({
-                  //   path: '/order/orderFormDetail',
-                  //   query: {
-                  //   }
-                  // });
-                },
               }
             });
           }
         }, {
-          label: '未提货量',
-          prop: 'notPickupGoodsAmount'
+          label: '供货商',
+          prop: 'supplierName',
+          width: 100
+        }, {
+          label: '订购数量',
+          prop: 'offerQty',
+          width: 80
+        }, {
+          label: '提货数量',
+          prop: 'pickupGoodsAmount',
+          width: 80,
+          render: (h, params) => {
+            return h({
+              template: '<div class="link-btn">{{data.row.pickupGoodsAmount}}</div>',
+              data() {
+                return {
+                  data: params
+                }
+              }
+            });
+          }
         }],
         tableData: [], //查询返回的数据
 
 
-        qryOpmOrderList: [], //查询返回的数据
         orderQueryData: {
           isCentman: '',
           offerNameOrCode: '',
@@ -133,6 +159,7 @@
           supplierId: '',
           statusCd: ''
         },
+
         isShowMoreCondition: false, //是否显示更多条件
         total: 0, //列表总数
         pageSize: 10, //每页展示条数
@@ -154,15 +181,16 @@
       qryOpmOrderPickupInfo(curPage, pageSize) {
         this.currentPage = curPage || 1;
         this.$post('/opmOrderController/queryOpmOrderPickupInfo', {
-          opMeetingId: '',
-          offerNameOrCode: '',
-          opmOrderNo: '',
-          retailerId: '',
-          supplierId: '',
+          opMeetingId: this.opMeetingInfo.opMeetingId,
+          isCentman: this.orderQueryData.isCentman,
+          offerNameOrCode: this.orderQueryData.offerNameOrCode,
+          opmOrderNo: this.orderQueryData.opmOrderNo,
+          retailerId: this.orderQueryData.retailerId,
+          supplierId: this.orderQueryData.supplierId,
           pageSize: pageSize || 10,
           curPage: curPage || 1
         }).then((rsp) => {
-          this.qryOpmOrderList = rsp.rows;
+          this.tableData = rsp.rows;
           this.total = rsp.totalSize;
         })
       },
@@ -173,11 +201,9 @@
     components: {
       Breadcrumb,
       Input,
-      Select,
       InputWithSelect,
       TitlePlate,
       Table,
-      DeviceInfo,
       Pagination,
       ChooseMerchants
     }
@@ -257,109 +283,5 @@
       line-height: 28px;
     }
 
-    .buttons .btns {
-      padding: 0 16px;
-      margin-left: 2px;
-      border: 0;
-      background-color: #fa0000;
-      color: #fff;
-      font-size: 12px;
-      border-radius: 3px;
-      line-height: 28px;
-      cursor: pointer;
-    }
-    .buttons .btns:hover {
-      background-color: #e20606;
-    }
-
-    .table thead tr {
-      height: 40px;
-      background-color: #efefef;
-      border: 1px solid #dcdcdc;
-      color: #131212;
-    }
-    .table tbody tr {
-      height: 90px;
-      border-bottom: 1px solid #dcdcdc;
-    }
-    .table thead tr th {
-      text-align: center;
-    }
-
-    .li-list {
-      margin-top: 16px;
-      border: 1px solid #e0e0e0;
-    }
-    .p-line {
-      height: 35px;
-      line-height: 35px;
-      background: #f8f8f8;
-      border-bottom: 1px solid #e0e0e0;
-      .date-color {
-        color: #807e7e;
-      }
-      span {
-        width: calc(33% - 20px);
-        padding: 0 10px;
-        b {
-          margin-right: 15px;
-        }
-      }
-    }
-    .tabs dl {
-      height: 90px;
-
-    }
-    .tabs .dll {
-      line-height: 90px;
-      text-align: center;
-    }
-    .wid32 {
-      width: 32%;
-    }
-    .wid17 {
-      width: 17%;
-    }
-    .wid11 {
-      width: 11%;
-    }
-    .wid10 {
-      width: 10%;
-    }
-    .wid13 {
-      width: 13%;
-    }
-    .updown-btn {
-      padding: 2px 5px;
-      border: 0;
-      text-decoration: underline;
-    }
-    .updown-btn:hover {
-      border: 1px solid #f82134;
-      border-radius: 3px;
-      text-decoration: none;
-      cursor: pointer;
-    }
-    .updown-btn.green:hover {
-      border: 1px solid #46b02e;
-    }
-    .red {
-      color: #f82134;
-    }
-    .text-center {
-      text-align: center;
-    }
-    .text-right {
-      text-align: right;
-    }
-    .pd5 {
-      padding: 5px;
-    }
-    .p-line span {
-      color: #aaa;
-    }
-    .p-line span b {
-      color: #333;
-    }
   }
 </style>
