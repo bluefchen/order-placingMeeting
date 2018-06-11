9b58c90567b56c1248484b6e17a3f5ea35e5aeb4
heyue <289383680@qq.com>
2018-06-11 10:20:03 +0800

    bugfix

--- src/util/axios.js
+++ src/util/axios.js
@@ -7,7 +7,7 @@ import qs from 'qs'
 
 axios.defaults.timeout = 5 * 1000;
 let user = JSON.parse(localStorage.getItem('user'));
-axios.defaults.baseURL = 'http://192.168.74.17:9086/';
+// axios.defaults.baseURL = 'http://192.168.74.17:9086/';
 axios.defaults.headers = {
   'Content-Type': 'application/x-www-form-urlencoded',
   'token': _.get(user, 'token')
