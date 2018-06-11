3f6c7f57880631ad95189b1e9e6a221ef00be021
聂亚兰 <3242662985@qq.com>
2018-06-11 14:12:38 +0800

    bugfix

--- src/views/SupplierDataMaintain.vue
+++ src/views/SupplierDataMaintain.vue
@@ -345,12 +345,12 @@
     /*中间背景图片*/
     .img-bg {
       width: 100%;
-      height: 401px;
+      height: 250px;
       background: #6e42ef;
     }
     .img-bg .info {
       width: 1200px;
-      height: 401px;
+      height: 250px;
       margin: 0 auto;
       background: #6e42ef url('../assets/images/manage-bg.jpg') no-repeat center bottom;
       overflow: hidden;
