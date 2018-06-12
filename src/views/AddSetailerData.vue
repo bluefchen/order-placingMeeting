8f7c26fb300c4c8ce806462e0f20cbe0fe83391f
heyue <289383680@qq.com>
2018-06-13 02:37:07 +0800

    bugfix

--- src/views/AddSetailerData.vue
+++ src/views/AddSetailerData.vue
@@ -1,73 +1,67 @@
 <template>
   <div class="v_supplier">
     <div class="box-1200">
-      <div class="order-titl fn-clear">
-        <TitlePlate class="fn-left" :title="title"/>
-      </div>
-      <div class="terminal-info-box">
-        <el-row :gutter="20">
-          <el-col :span="8" :offset="2">
-            <div class="condition-item">
-              <label class="label-wrds text-right"><span class="red-star">*</span> 所属省份：</label>
-              <AreaSelect :value.sync="retailerInfo.commonRegionId"/>
-            </div>
-          </el-col>
-        </el-row>
-        <el-row :gutter="20">
-          <el-col :span="8" :offset="2">
-            <div class="condition-item">
-              <label class="label-wrds text-right"><span class="red-star">*</span> 零售商名称：</label>
-              <Input :value.sync="retailerInfo.retailerName"/>
-            </div>
-          </el-col>
-          <el-col :span="8" :offset="2">
-            <div class="condition-item">
-              <label class="label-wrds text-right"><span class="red-star">*</span> 零售商类型：</label>
-              <Select :value.sync="retailerInfo.retailerType" :options="retailerTypeList"/>
-            </div>
-          </el-col>
-        </el-row>
-        <el-row :gutter="20">
-          <el-col :span="8" :offset="2">
-            <div class="condition-item">
-              <label class="label-wrds text-right">联系人：</label>
-              <Input :value.sync="retailerInfo.linkMan"/>
-            </div>
-          </el-col>
-          <el-col :span="8" :offset="2">
-            <div class="condition-item">
-              <label class="label-wrds text-right">联系人手机：</label>
-              <Input :value.sync="retailerInfo.linkNbr"/>
-            </div>
-          </el-col>
-        </el-row>
-        <el-row :gutter="20">
-          <el-col :span="8" :offset="2">
-            <div class="condition-item">
-              <label class="label-wrds text-right">公司电话：</label>
-              <Input :value.sync="retailerInfo.retailerPhone"/>
-            </div>
-          </el-col>
-          <el-col :span="8" :offset="2">
-            <div class="condition-item">
-              <label class="label-wrds text-right">公司传真：</label>
-              <Input :value.sync="retailerInfo.retailerFax"/>
-            </div>
-          </el-col>
-        </el-row>
-        <el-row :gutter="20">
-          <el-col :span="18" :offset="2">
-            <div class="condition-item">
-              <label class="label-wrds text-right">备注：</label>
-              <Input type="textarea" v-model="retailerInfo.remarks" />
-            </div>
-          </el-col>
-        </el-row>
-      </div>
-      <div class="foot-btn">
-        <button class="btns" :disabled="!retailerInfo.commonRegionId || !retailerInfo.retailerName || !retailerInfo.retailerType" @click="saveSetailerData">保&nbsp;存</button>
-        <button class="btns" @click="cancelSetailerData">取&nbsp;消</button>
-      </div>
+      <el-form :model="retailerInfo"  :rules="rules" ref="retailerInfo" label-width="110px" class="demo-ruleForm">
+        <div class="order-titl fn-clear">
+          <TitlePlate class="fn-left" :title="title"/>
+        </div>
+        <div class="terminal-info-box">
+          <el-row :gutter="20">
+            <el-col :span="8" :offset="2">
+              <el-form-item label="所属省份：" prop="commonRegionId">
+                <AreaSelect :value.sync="retailerInfo.commonRegionId"/>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row :gutter="20">
+            <el-col :span="8" :offset="2">
+              <el-form-item label="零售商名称：" prop="retailerName">
+                <Input :value.sync="retailerInfo.retailerName"/>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8" :offset="2">
+              <el-form-item label="零售商类型：" prop="retailerType">
+                <Select :value.sync="retailerInfo.retailerType" :options="retailerTypeList"/>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row :gutter="20">
+            <el-col :span="8" :offset="2">
+              <el-form-item label="联系人：" prop="linkMan">
+                <Input :value.sync="retailerInfo.linkMan"/>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8" :offset="2">
+              <el-form-item label="联系人手机：" prop="linkNbr">
+                <Input :value.sync="retailerInfo.linkNbr"/>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row :gutter="20">
+            <el-col :span="8" :offset="2">
+              <el-form-item label="公司电话：" prop="retailerPhone">
+                <Input :value.sync="retailerInfo.retailerPhone"/>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8" :offset="2">
+              <el-form-item label="公司传真：" prop="retailerFax">
+                <Input :value.sync="retailerInfo.retailerFax"/>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row :gutter="20">
+            <el-col :span="18" :offset="2">
+              <el-form-item label="备注：" prop="remarks">
+                <Input type="textarea" :value.sync="retailerInfo.remarks" />
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </div>
+        <div class="foot-btn">
+          <button class="btns" @click="submitForm('retailerInfo')">保&nbsp;存</button>
+          <button class="btns" @click="cancelSetailerData">取&nbsp;消</button>
+        </div>
+      </el-form>
     </div>
   </div>
 </template>
@@ -90,6 +84,28 @@
       };
     },
     data() {
+      var checkPhone = (rule, value, callback) => {
+        if (value) {
+          let reg = /^(\+86)?1[345789]\d{9}$/;
+          if (!(reg.test(value))) {
+            callback(new Error('请输入正确的手机号码'));
+          }
+          callback();
+        }else{
+          callback();
+        }
+      };
+      var checkTel = (rule, value, callback) => {
+        if (value) {
+          let reg = /^0\d{2,3}-?\d{7,8}$/;
+          if (!(reg.test(value))) {
+            callback(new Error('请输入正确的电话号码'));
+          }
+          callback();
+        }else{
+          callback();
+        }
+      };
       return {
         title: '',
         level: 'province',
@@ -104,12 +120,49 @@
         },{
           value: '1003',
           label: '代理商'
-        }]
+        }],
+
+        rules: {
+          commonRegionId: [
+            {required: true, message: '请选择所属省份', trigger: 'change'}
+          ],
+          retailerName: [
+            {required: true, message: '零售商名称不能为空', trigger: 'blur'},
+            {min: 0, max: 200, message: '长度不能超过200个字符', trigger: 'change'}
+          ],
+          retailerType: [
+            {required: true, message: '零售商名称不能为空', trigger: 'change'}
+          ],
+          linkMan: [
+            {min: 0, max: 20, message: '长度不能超过20个字符', trigger: 'blur'}
+          ],
+          linkNbr: [
+            {validator: checkPhone, message: '请输入正确手机号码', trigger: 'blur'}
+          ],
+          retailerPhone: [
+            {validator: checkTel, message: '请输入正确的公司号码', trigger: 'blur'}
+          ],
+          retailerFax: [
+            {validator: checkTel, message: '请输入正确的公司传真', trigger: 'blur'}
+          ],
+          remarks: [
+            {min: 0, max: 200, message: '长度不能超过200个字符', trigger: 'blur'}
+          ]
+        },
       }
     },
     methods: {
       handleChange(val){
         this.retailerInfo.commonRegionId = val;
+      },
+      submitForm(formName, title) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            this.saveSetailerData();
+          } else {
+            return false;
+          }
+        });
       },
       saveSetailerData(){
         if(this.title === '修改零售商'){
@@ -229,6 +282,37 @@
         }
       }
     }
+    //表单提示样式
+    .el-form-item{
+      margin: 10px 0;
+    }
+
+    .editor-error{
+      position: absolute;
+      left: 0;
+      bottom: -30px;
+      padding: 5px;
+      z-index: 999;
+      color: #f56c6c;
+      font-size: 12px;
+      line-height: 1;
+      padding-top: 4px;
+    }
+    .el-form-item__error{
+      padding: 5px;
+      z-index: 999;
+    }
+
+    .el-form-item__label{
+      height: 30px;
+      line-height: 30px;
+      padding: 0;
+      color: #333;
+    }
+    .el-form-item__content{
+      line-height: 30px;
+    }
+    //表单提示样式
   }
   .el-input.is-active .el-input__inner, .el-input__inner:focus {
     border-color: #ff7a7a;
