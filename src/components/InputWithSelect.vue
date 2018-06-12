d7a56b578f9f9eb89aacf952757454f46783e804
聂亚兰 <3242662985@qq.com>
2018-06-13 02:14:43 +0800

    bugfix

--- src/components/InputWithSelect.vue
+++ src/components/InputWithSelect.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="v_input-with-select">
-    <el-input placeholder="输入终端编码或名称搜索" v-model="input" class="input-with-select" :size="size">
+    <el-input :placeholder="placeholder" v-model="input" class="input-with-select" :size="size">
       <el-select v-model="select" slot="prepend" placeholder="请选择" v-if="!isHideSelect">
         <el-option
           v-for="item in options"
@@ -21,6 +21,10 @@
       size: {
         type: String,
         default: 'small'
+      },
+      placeholder: {
+        type: String,
+        default: '输入终端编码或名称搜索'
       },
       options: {
         type: Array,
