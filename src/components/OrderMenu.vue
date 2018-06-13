95186ef2f7207f03706834c4cef5d10cbbb1b880
dingshaohua <345561171@qq.com>
2018-06-14 02:08:11 +0800

    bugfix 根据订货会状态配置菜单

--- src/components/OrderMenu.vue
+++ src/components/OrderMenu.vue
@@ -2,39 +2,39 @@
   <div class="v_order-menu">
     <el-menu :default-active="$route.path" class="fn-right" mode="horizontal" router>
       <el-menu-item index="/order/orderIndex">首页</el-menu-item>
-      <el-submenu index="2" :disabled="this.opMeetingInfo.statusCd === '1002'">
+      <el-submenu index="2">
         <template slot="title">机型管理</template>
-        <el-menu-item index="/order/orderModel">机型维护</el-menu-item>
+        <el-menu-item index="/order/orderModel" :disabled="this.opMeetingInfo.statusCd === '1001' || this.opMeetingInfo.statusCd === '1002'">机型维护</el-menu-item>
         <el-menu-item index="/order/importModelAdd" style="display: none;">机型导入新增</el-menu-item>
         <el-menu-item index="/order/importModelDel" style="display: none;">机型导入删除</el-menu-item>
-        <el-menu-item index="/order/specialModel">特种机型分配量维护</el-menu-item>
+        <el-menu-item index="/order/specialModel" :disabled="this.opMeetingInfo.statusCd === '1001' || this.opMeetingInfo.statusCd === '1002'">特种机型分配量维护</el-menu-item>
         <el-menu-item index="/order/importSpecialModel" style="display: none;">特种机型导入新增</el-menu-item>
       </el-submenu>
-      <el-submenu index="3" :disabled="this.opMeetingInfo.statusCd === '1002'">
+      <el-submenu index="3">
         <template slot="title">订单管理</template>
-        <el-menu-item index="/order/importPickupAdd">订单批量导入</el-menu-item>
-        <el-menu-item index="/order/orderPickupData">订单提货数据维护</el-menu-item>
+        <el-menu-item index="/order/importPickupAdd" :disabled="this.opMeetingInfo.statusCd === '1001' || this.opMeetingInfo.statusCd === '1002'">订单批量导入</el-menu-item>
+        <el-menu-item index="/order/orderPickupData" :disabled="this.opMeetingInfo.statusCd === '1000'">订单提货数据维护</el-menu-item>
         <el-menu-item index="/order/importPickupData" style="display: none;">订单提货数据导入</el-menu-item>
         <el-menu-item index="/order/orderPickupGoodsConfirm" style="display: none;">订单提货确认</el-menu-item>
       </el-submenu>
-      <el-submenu index="4" :disabled="this.opMeetingInfo.statusCd === '1002'">
+      <el-submenu index="4">
         <template slot="title">定金管理</template>
-        <el-menu-item index="/order/depositConfigure">定金配置</el-menu-item>
-        <el-menu-item index="/order/depositAddRecord">定金补录</el-menu-item>
-        <el-menu-item index="/order/cyjDepositAddRecord">诚意金补录</el-menu-item>
+        <el-menu-item index="/order/depositConfigure" :disabled="this.opMeetingInfo.statusCd === '1001' || this.opMeetingInfo.statusCd === '1002'">定金配置</el-menu-item>
+        <el-menu-item index="/order/depositAddRecord" :disabled="this.opMeetingInfo.statusCd === '1000'">定金补录</el-menu-item>
+        <el-menu-item index="/order/cyjDepositAddRecord" :disabled="this.opMeetingInfo.statusCd === '1000'">诚意金补录</el-menu-item>
       </el-submenu>
-      <el-submenu index="5" :disabled="this.opMeetingInfo.statusCd === '1002'">
+      <el-submenu index="5">
         <template slot="title">政策管理</template>
-        <el-menu-item index="/order/policyManage">政策投入</el-menu-item>
+        <el-menu-item index="/order/policyManage" :disabled="this.opMeetingInfo.statusCd === '1001' || this.opMeetingInfo.statusCd === '1002'">政策投入</el-menu-item>
         <el-menu-item index="/order/importPolicyAdd" style="display: none;">上传政策</el-menu-item>
-        <el-menu-item index="/order/policyList">政策列表</el-menu-item>
-        <el-menu-item index="/order/policyExamine">政策审核</el-menu-item>
+        <el-menu-item index="/order/policyList" :disabled="this.opMeetingInfo.statusCd === '1001' || this.opMeetingInfo.statusCd === '1002'">政策列表</el-menu-item>
+        <el-menu-item index="/order/policyExamine" :disabled="this.opMeetingInfo.statusCd === '1001' || this.opMeetingInfo.statusCd === '1002'">政策审核</el-menu-item>
       </el-submenu>
       <el-submenu index="/order/orderCompositeQuery">
         <template slot="title">统计查询</template>
         <el-menu-item index="/order/orderCompositeQuery">订单综合查询</el-menu-item>
         <el-menu-item index="/order/orderdetail" style="display: none;">订单详情</el-menu-item>
-        <el-menu-item index="/order/preferentialPolicySettlement">优惠政策结算</el-menu-item>
+        <el-menu-item index="/order/preferentialPolicySettlement" :disabled="this.opMeetingInfo.statusCd === '1000'">优惠政策结算</el-menu-item>
         <el-menu-item index="/order/terminalQuery">终端查询</el-menu-item>
         <el-menu-item index="/order/summaryStatisticalAnalysis">全国汇总统计分析</el-menu-item>
         <el-menu-item index="/order/summaryAnalysis">汇总统计</el-menu-item>
